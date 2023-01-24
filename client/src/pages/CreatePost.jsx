b4c0db9999bfeeb26e74466ebe63f219ceab62e0
baberparweez <baberparweez@gmail.com>
2023-01-24 15:46:56 -0300

    add client folder and basic setup with Home and CreatePosts

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
+
+const CreatePost = () => {
+    return <div>Create Post</div>;
+};
+
+export default CreatePost;
---
+++ client/src/utils/index.js
@@ -0,0 +1,8 @@
+import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompts(prompt) {
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const randomPrompt = surpriseMePrompts[randomIndex];
+
+    return randomPrompt;
+}
